769e036726e8b6362270971b4403a28a4093bec2
kilexDDD <162269934+kilexDDD@users.noreply.github.com>
2024-03-09 16:25:43 -0500

    Update audio.lua

--- audio.lua
+++ audio.lua
@@ -1,6 +1,6 @@
 _G.toggled = true
 while _G.toggled == true do
-for i, tool in pairs(game.Workspace.LocalPlayer:GetDescendants()) do
+for i, tool in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
 if tool:IsA("Tool") and tool:FindFirstChild("Handle") then
 tool.Handle.Sound.Playing = true
 tool.Handle.Sound.TimePosition = math.random(5,120) 

28f9e8145d35959212c0410dc7ea3e0f1c346d96
kilexDDD <162269934+kilexDDD@users.noreply.github.com>
2024-03-10 19:10:12 -0400

    Update and rename boomboxhubREmasterd.lua to aimbot

--- boomboxhubREmasterd.lua
+++
@@ -1,2 +0,0 @@
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("i wuhv litle girls :3", "All")
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("IM ANTHONY IM A PDF PHILE!!!", "All")
